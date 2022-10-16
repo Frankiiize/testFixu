08735b38ca1c5033f9a3c88cb5b74d0d7721494e
frankiiize <frankiiize@gmail.com>
2022-10-16 12:49:23 -0300

    Feature: card  form component -- added styles -- added html -- material io imports

--- src/js/materialImports.js
+++ src/js/materialImports.js
@@ -1,5 +1,19 @@
 import {MDCRipple} from '@material/ripple';
 import {MDCTextField} from '@material/textfield';
+import {MDCCheckbox} from '@material/checkbox';
 
-const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
+const fileds = [
+  '.username',
+  '.lastName',
+  '.email',
+  '.phone',
+  '.rut'
+]
+const fieldForm = document.querySelectorAll('.mdc-text-field')
+const fieldFormAdd = fileds.map.call(fieldForm, function(el) {
+  return new MDCTextField(el);
+});
+console.log(fieldFormAdd)
+
 const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
+const checkbox = new MDCCheckbox(document.querySelector('.mdc-checkbox'));
